ba11371835f47cb71f332c57b0cc28a21a192990
ToniIAPro73 <supertoniia@gmail.com>
2026-02-25 23:30:30 +0100

    Reduce mobile unused CSS by excluding unused ui components from Tailwind scan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    '!./src/components/ui/**/*',
+  ],
   theme: {
     extend: {
       colors: {
